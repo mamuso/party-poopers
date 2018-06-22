95702c68388463547b1016740afa7ec7b78dc409
mamuso <mamuso@mamuso.net>
2018-06-22 00:13:18 -0700

    Starting the simulation

---
+++ simulation.js
@@ -0,0 +1,8 @@
+const shuffle = require('shuffle-array');
+
+// Deck of party goodies and party poopers 
+const goodies = Array(20);
+goodies.fill(1);
+
+const poopers = Array(20);
+poopers.fill(-1);
