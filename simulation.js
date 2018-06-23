53f1f724af3085e82edd4e51bbd7f0385e591591
mamuso <mamuso@mamuso.net>
2018-06-22 23:19:08 -0700

    Distributing goodies and poopers

--- simulation.js
+++ simulation.js
@@ -2,8 +2,17 @@ const Shuffle = require('shuffle-array');
 const Roll = require('roll');
 
 // Deck of party goodies and party poopers 
-const goodies = Array(20);
+const goodies = Array(18);
 goodies.fill(1);
 
-const poopers = Array(20);
+const supergoodies = Array(2);
+supergoodies.fill(2);
+
+const poopers = Array(15);
 poopers.fill(-1);
+
+const superpoopers = Array(5);
+superpoopers.fill(-2);
+
+const deck = [...goodies, ...supergoodies, ...poopers, ...superpoopers];
+console.log(Shuffle(deck));
