652ac9f93518f563cc18b3d484867b2d3b297a48
mamuso <mamuso@mamuso.net>
2018-07-10 23:53:18 -0700

    keep playing and setting turns

--- simulation.js
+++ simulation.js
@@ -1,5 +1,5 @@
-const Shuffle = require('shuffle-array');
-const Roll = require('roll');
+const Shuffle = require("shuffle-array");
+const Roll = require("roll");
 
 // Defining the deck
 const goodiesCards = 18;
@@ -14,20 +14,17 @@ const superpoopers = Array(superpoopersCards).fill(-2);
 
 const deck = [...goodies, ...supergoodies, ...poopers, ...superpoopers];
 
-// We have a set of 12 Birdie Wirdies, with numbers on top (1 to 6). 
+// We have a set of 12 Birdie Wirdies, with numbers on top (1 to 6).
 // Each player will get 3 during the game set up (max 4 players)
 const birdiewirdiesdeck = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6];
-
 
 // Stats
 let statHands = 0;
 let statMaxCoins = 0;
 
-
 // Let's define all the game rules
 class Game {
   constructor(players, deck, birdiewirdies) {
-
     // Get all the game ingredients
     this.players = Array(players);
     this.deck = Shuffle(deck);
@@ -37,7 +34,7 @@ class Game {
     // Set up the game: 3 birds per player, 3 energy coins per bird
     for (let i = 0; i < players; i++) {
       let j = 0;
-      this.players[i] = Array()
+      this.players[i] = Array();
       while (j < 3) {
         this.players[i][j] = [this.birdiewirdies.pop(), 3];
         j++;
@@ -45,14 +42,15 @@ class Game {
     }
 
     this.play();
-  };
+  }
 
   play() {
     while (this.keepPlaying()) {
-      console.log(this.setTurn())
+      // setting the turn of the next player
+      this.setTurn();
       statHands++;
     }
-  };
+  }
 
   // Who plays next
   setTurn() {
@@ -64,18 +62,14 @@ class Game {
   }
 
   keepPlaying() {
-    // this.players()
-    if (statHands < 100) {
+    if (this.players() > 1) {
       return true;
     } else {
       return false;
     }
-  };
-
-  printStats() {
-
   }
 
+  printStats() {}
 }
 
 const game1 = new Game(2, deck, birdiewirdiesdeck);
